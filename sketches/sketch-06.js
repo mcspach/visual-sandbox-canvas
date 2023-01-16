403a8b4813e626bfc717a73ff6a352591a70d3ca
Matthew Spach <matthews@anyfans.com>
2023-01-15 21:10:51 -0800

    added letter and centered on canvas

--- sketches/sketch-06.js
+++ sketches/sketch-06.js
@@ -1,13 +1,30 @@
 const canvasSketch = require('canvas-sketch');
 
 const settings = {
-  dimensions: [ 2048, 2048 ]
+  dimensions: [ 1080, 1080 ]
 };
 
 const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
+
+    context.fillStyle = 'black';
+    context.font = '1200px serif';
+    context.textBaseline = 'middle';
+    context.textAlign = 'center';
+
+    const text = 'A';
+
+    const metrics = context.measureText(text);
+
+    
+
+    context.save;
+    context.translate(width * 0.5, height * 0.5);
+    context.fillText(text, 0, 0);
+    context.restore;
+
   };
 };
 
