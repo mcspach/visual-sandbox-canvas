9c96d945a21fe537c621cfb5b4633c9e1d4cc848
Matthew Spach <matthews@anyfans.com>
2023-02-03 11:18:12 -0800

    finished 07

--- sketches/sketch-07.js
+++ sketches/sketch-07.js
@@ -1,4 +1,5 @@
 const canvasSketch = require('canvas-sketch');
+const random = require('canvas-sketch-util/random');
 
 const settings = {
   dimensions: [ 1080, 1080 ],
@@ -55,6 +56,12 @@ const sketch = ({ context, width, height }) => {
     console.log(typeData);
     context.drawImage(typeCanvas, 0, 0);
 
+    context.fillStyle = 'black';
+    context.fillRect(0, 0, width, height);
+    context.textBaseline = 'middle';
+    context.textAlign = 'center';
+
+
     for (let i = 0; i < numCells; i++) {
       const col = i % cols;
       const row = Math.floor(i / cols);
@@ -67,19 +74,41 @@ const sketch = ({ context, width, height }) => {
       const b = typeData[i * 4 + 2];
       const a = typeData[i * 4 + 3];
 
-      context.fillStyle = `rgb(${r}, ${g}, ${b})`;
+      // context.fillStyle = `rgb(${r}, ${g}, ${b})`;
+      context.fillStyle = 'white';
+
+      context.font = `${cell * 2}px ${fontFamily}`;
+      if (Math.random() < 0.1) context.font = `${cell * 7}px ${fontFamily}`;
+
+      const glyph = getGlyph(r);
 
       context.save();
       context.translate(x, y);
       context.translate(cell*0.5, cell*0.5);
-      context.beginPath();
-      context.arc(0,0,cell*0.5,0,Math.PI*2);
-      context.fill();
+
+      // context.beginPath();
+      // context.arc(0,0,cell*.5,0,Math.PI*2);
+      // context.fill();
       // context.fillRect(0, 0, cell, cell);
+
+      context.fillText(glyph, 0, 0);
+
+
+
       context.restore();
     }
   };
 };
+
+const getGlyph = (v) => {
+  if (v < 50) return '';
+  if (v < 100) return '.';
+  if (v < 150) return '-';
+  if (v < 200) return '+'; 
+
+  const glyphs = '_= /'.split('');
+  return random.pick(glyphs);
+}
 
 //converts the canvas to a dynamic state where canvasSketch reRenders after each keystroke.
 const onKeyUp = (e) => {
