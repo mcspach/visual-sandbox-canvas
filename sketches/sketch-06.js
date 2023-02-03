10315dc2893867c6ff55f9212ceda45cc9a4178b
Matthew Spach <matthews@anyfans.com>
2023-02-03 09:10:29 -0800

    centered text on canvas sketch 6

--- sketches/sketch-06.js
+++ sketches/sketch-06.js
@@ -11,20 +11,31 @@ const sketch = () => {
 
     context.fillStyle = 'black';
     context.font = '1200px serif';
-    context.textBaseline = 'middle';
+    context.textBaseline = 'top';
     context.textAlign = 'center';
 
     const text = 'A';
 
     const metrics = context.measureText(text);
+    console.log(metrics);
+    const mx = metrics.actualBoundingBoxLeft * -1;
+    const my = metrics.actualBoundingBoxAscent * -1;
+    const mw = metrics.actualBoundingBoxLeft + metrics.actualBoundingBoxRight;
+    const mh = metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent;
 
+    const x = (width - mw) * 0.5 - mx;
+    const y = (width - mh) * 0.5 - my;
+  
+    context.save();
+    context.translate(x, y);
+
+    context.beginPath();
+    context.rect(mx, my, mw, mh);
+    context.stroke();
+
+    context.fillText('A', 0, 0);
+    context.restore();
     
-
-    context.save;
-    context.translate(width * 0.5, height * 0.5);
-    context.fillText(text, 0, 0);
-    context.restore;
-
   };
 };
 
